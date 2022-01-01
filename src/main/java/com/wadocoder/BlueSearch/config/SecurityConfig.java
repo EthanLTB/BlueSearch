0b37bc30bd282c5319d51268253a78810f14564c
Ethan <66878322+EthanLTB@users.noreply.github.com>
2022-01-01 17:55:53 -0500

    More security configuration
    nothing special just continued adding spring security and jwt

--- src/main/java/com/wadocoder/BlueSearch/config/SecurityConfig.java
+++ src/main/java/com/wadocoder/BlueSearch/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.wadocoder.BlueSearch.config;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.wadocoder.BlueSearch.filter.JwtFilter;
 import com.wadocoder.BlueSearch.service.UserDetailsServiceImpl;
 import com.wadocoder.BlueSearch.util.CustomPasswordEncoder;
 
@@ -16,15 +21,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	private UserDetailsServiceImpl userDetailsService;
 	@Autowired
 	private CustomPasswordEncoder passwordEncoder;
+	@Autowired
+	private JwtFilter jwtFilter;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 	auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder.getPasswordEncoder());
 	}
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http = http.csrf().disable().cors().disable();
+        
+        http = http.sessionManagement()
+                   .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                   .and();
+        
+        http = http.exceptionHandling()
+                   .authenticationEntryPoint((request, response, ex) -> {
+                       response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+                   }).and();
+                   
+        http.authorizeRequests()
+            .antMatchers("/api/auth/**").permitAll()
+            .anyRequest().authenticated();
+        
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        
+    }
 }
