43d7e8f4c9567cd9d1b3ed726595b941df6df064
Ethan <66878322+EthanLTB@users.noreply.github.com>
2022-01-30 13:53:43 -0500

    working on deploy

---
+++ src/main/java/com/wadocoder/BlueSearch/dto/Trade.java
@@ -0,0 +1,14 @@
+package com.wadocoder.BlueSearch.dto;
+
+public class Trade {
+private boolean hasTrade;
+
+public boolean isHasTrade() {
+	return hasTrade;
+}
+
+public void setHasTrade(boolean hasTrade) {
+	this.hasTrade = hasTrade;
+} 
+
+}
